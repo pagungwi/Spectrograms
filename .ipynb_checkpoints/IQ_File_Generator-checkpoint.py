7009e044b473756a2e14602836b576e32658358b
sireinders <sam.i.reinders@gmail.com>
2025-11-02 15:21:52 -0500

    Generated all IQ sample subsets for DOP, FSF

---
+++ .ipynb_checkpoints/IQ_File_Generator-checkpoint.py
@@ -0,0 +1,148 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#
+# SPDX-License-Identifier: GPL-3.0
+#
+# GNU Radio Python Flow Graph
+# Title: Not titled yet
+# GNU Radio version: 3.10.12.0
+
+from PyQt5 import Qt
+from gnuradio import qtgui
+from gnuradio import analog
+from gnuradio import blocks
+from gnuradio import channels
+from gnuradio.filter import firdes
+from gnuradio import gr
+from gnuradio.fft import window
+import sys
+import signal
+from PyQt5 import Qt
+from argparse import ArgumentParser
+from gnuradio.eng_arg import eng_float, intx
+from gnuradio import eng_notation
+import threading
+
+
+
+class IQ_File_Generator(gr.top_block, Qt.QWidget):
+
+    def __init__(self):
+        gr.top_block.__init__(self, "Not titled yet", catch_exceptions=True)
+        Qt.QWidget.__init__(self)
+        self.setWindowTitle("Not titled yet")
+        qtgui.util.check_set_qss()
+        try:
+            self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
+        except BaseException as exc:
+            print(f"Qt GUI: Could not set Icon: {str(exc)}", file=sys.stderr)
+        self.top_scroll_layout = Qt.QVBoxLayout()
+        self.setLayout(self.top_scroll_layout)
+        self.top_scroll = Qt.QScrollArea()
+        self.top_scroll.setFrameStyle(Qt.QFrame.NoFrame)
+        self.top_scroll_layout.addWidget(self.top_scroll)
+        self.top_scroll.setWidgetResizable(True)
+        self.top_widget = Qt.QWidget()
+        self.top_scroll.setWidget(self.top_widget)
+        self.top_layout = Qt.QVBoxLayout(self.top_widget)
+        self.top_grid_layout = Qt.QGridLayout()
+        self.top_layout.addLayout(self.top_grid_layout)
+
+        self.settings = Qt.QSettings("gnuradio/flowgraphs", "IQ_File_Generator")
+
+        try:
+            geometry = self.settings.value("geometry")
+            if geometry:
+                self.restoreGeometry(geometry)
+        except BaseException as exc:
+            print(f"Qt GUI: Could not restore geometry: {str(exc)}", file=sys.stderr)
+        self.flowgraph_started = threading.Event()
+
+        ##################################################
+        # Variables
+        ##################################################
+        self.samp_rate = samp_rate = 23.04e6
+
+        ##################################################
+        # Blocks
+        ##################################################
+
+        self.channels_channel_model_0 = channels.channel_model(
+            noise_voltage=0.1,
+            frequency_offset=0.0,
+            epsilon=1.0,
+            taps=[1.0],
+            noise_seed=42,
+            block_tags=False)
+        self.blocks_throttle2_0_0 = blocks.throttle( gr.sizeof_gr_complex*1, samp_rate, True, 0 if "auto" == "auto" else max( int(float(0.1) * samp_rate) if "auto" == "time" else int(0.1), 1) )
+        self.blocks_throttle2_0 = blocks.throttle( gr.sizeof_gr_complex*1, samp_rate, True, 0 if "auto" == "auto" else max( int(float(0.1) * samp_rate) if "auto" == "time" else int(0.1), 1) )
+        self.blocks_head_0_0 = blocks.head(gr.sizeof_gr_complex*1, 1024)
+        self.blocks_head_0 = blocks.head(gr.sizeof_gr_complex*1, 1024)
+        self.blocks_file_sink_0_0 = blocks.file_sink(gr.sizeof_gr_complex*1, 'post_channel.iq', False)
+        self.blocks_file_sink_0_0.set_unbuffered(False)
+        self.blocks_file_sink_0 = blocks.file_sink(gr.sizeof_gr_complex*1, 'pre_channel.iq', False)
+        self.blocks_file_sink_0.set_unbuffered(False)
+        self.analog_sig_source_x_0 = analog.sig_source_c(samp_rate, analog.GR_SAW_WAVE, 1842.5e6, 1, 0, 0)
+
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.connect((self.analog_sig_source_x_0, 0), (self.blocks_throttle2_0, 0))
+        self.connect((self.analog_sig_source_x_0, 0), (self.channels_channel_model_0, 0))
+        self.connect((self.blocks_head_0, 0), (self.blocks_file_sink_0, 0))
+        self.connect((self.blocks_head_0_0, 0), (self.blocks_file_sink_0_0, 0))
+        self.connect((self.blocks_throttle2_0, 0), (self.blocks_head_0, 0))
+        self.connect((self.blocks_throttle2_0_0, 0), (self.blocks_head_0_0, 0))
+        self.connect((self.channels_channel_model_0, 0), (self.blocks_throttle2_0_0, 0))
+
+
+    def closeEvent(self, event):
+        self.settings = Qt.QSettings("gnuradio/flowgraphs", "IQ_File_Generator")
+        self.settings.setValue("geometry", self.saveGeometry())
+        self.stop()
+        self.wait()
+
+        event.accept()
+
+    def get_samp_rate(self):
+        return self.samp_rate
+
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+        self.analog_sig_source_x_0.set_sampling_freq(self.samp_rate)
+        self.blocks_throttle2_0.set_sample_rate(self.samp_rate)
+        self.blocks_throttle2_0_0.set_sample_rate(self.samp_rate)
+
+
+
+
+def main(top_block_cls=IQ_File_Generator, options=None):
+
+    qapp = Qt.QApplication(sys.argv)
+
+    tb = top_block_cls()
+
+    tb.start()
+    tb.flowgraph_started.set()
+
+    tb.show()
+
+    def sig_handler(sig=None, frame=None):
+        tb.stop()
+        tb.wait()
+
+        Qt.QApplication.quit()
+
+    signal.signal(signal.SIGINT, sig_handler)
+    signal.signal(signal.SIGTERM, sig_handler)
+
+    timer = Qt.QTimer()
+    timer.start(500)
+    timer.timeout.connect(lambda: None)
+
+    qapp.exec_()
+
+if __name__ == '__main__':
+    main()
---
+++ .ipynb_checkpoints/compare-checkpoint.cpp
@@ -0,0 +1,144 @@
+#include <iostream>
+#include <cstdio>
+#include <cassert>
+#include <cstdio> 
+#include <string> 
+#include <opencv2/opencv.hpp> 
+#include <cmath> 
+#include <iomanip>
+#include <chrono>
+#include <stdlib.h>
+#include <string.h>
+#include <sstream>
+/* 
+Serially compare each pixel by computing pixel difference to generate spectrogram of channel 
+*/
+unsigned char * serialCompare(const unsigned char* pre, const unsigned char* post, size_t numPixels) {
+    unsigned char * chnl = (unsigned char *)malloc(numPixels*4*sizeof(unsigned char));
+    for (size_t i = 0; i < numPixels; ++i) {  // 4 channels per pixel (RGBA)
+        size_t idx = i*4;
+        chnl[idx] = post[idx] - pre[idx]; // R
+        chnl[idx+1] = post[idx+1] - pre[idx+1]; // G
+        chnl[idx+2] = post[idx+2] - pre[idx+2]; // B
+        chnl[idx+3] = post[idx+3] - pre[idx+3]; // A
+    }
+    return chnl;
+}
+
+/* 
+Parallelized computation of pixel difference to generate spectrogram of channel (OpenMP) --> Precious
+*/
+unsigned char * parallelCompare(const unsigned char* pre, const unsigned char* post, size_t numPixels) {
+    unsigned char * chnl = (unsigned char *)malloc(numPixels*4*sizeof(unsigned char));
+    // size_t diffCount = 0;
+
+    // for (size_t i = 0; i < numPixels; ++i) {
+    //     size_t idx = i * 4;  // 4 channels per pixel (RGBA)
+    //     if (img1[idx + 0] != img2[idx + 0] ||  // R
+    //         img1[idx + 1] != img2[idx + 1] ||  // G
+    //         img1[idx + 2] != img2[idx + 2] ||  // B
+    //         img1[idx + 3] != img2[idx + 3]) {  // A
+    //         ++diffCount;
+    //     }
+    // }
+    
+    // if (diffCount == 0) {
+    // std::cout << "Images are the same\n";   
+    // } else {
+    // std::cout << "Images are not the same.\n Number of differing pixels: " << diffCount << std::endl;
+    // }
+    return chnl;
+}
+
+/* 
+Vectorized computation of pixel difference to generate spectrogram of channel (AVX512 registers) --> Sam
+*/
+unsigned char * vectorizedCompare(const unsigned char* pre, const unsigned char* post, size_t numPixels) {
+    unsigned char * chnl = (unsigned char *)malloc(numPixels*4*sizeof(unsigned char));
+    
+    return chnl;
+}
+
+/* Verify correctness across approaches */
+bool diffCheck(const unsigned char * ch1, const unsigned char * ch2) {
+    bool indicator;
+    return false;
+}
+
+/*
+Kernel that creates channel image for human verification
+*/
+void createChannelImage(unsigned char *in, const int rows, const int cols, std::string name)
+{
+    cv::Mat output(rows, cols, CV_8UC4, in); // generate image
+    bool suc = cv::imwrite(name.c_str(), output);
+    if(!suc){
+        std::cerr << "Couldn't write Channel image!\n";
+        exit(1);
+    }
+}
+
+
+int main(int argc, char const *argv[]) {
+    
+    int im_rows, im_cols, cmp_type;
+    std::string chnl_type;
+
+    switch(argc){
+        case 1:
+            im_rows = 256;
+            im_cols = 256;
+            cmp_type = 0;
+            chnl_type = "AWGN";
+            break; 
+        case 5:
+            im_rows = atoi(argv[1]);
+            im_cols = atoi(argv[2]);
+            cmp_type = atoi(argv[3]);
+            chnl_type = argv[4];
+            break;
+
+        default: 
+                std::cerr << "Usage: rows columns compare_type channel_type\nValid compare types: \nserial=0 \nparallel=1 \nvectorized=2\n";
+                exit(1);
+    }
+    
+    const size_t  numPixels = im_rows*im_cols;
+    std::cout << "Rows: " << im_rows << std::endl;
+    std::cout << "Cols: " << im_cols << std::endl;
+    std::cout << "Channel Type: " << chnl_type << std::endl;
+    std::cout << "Size of channel image: " << numPixels*4*sizeof(unsigned char) << std::endl;
+    
+    //Read in image at specified file path
+    std::ostringstream oss;
+    oss << im_rows << "x" << im_cols << "-" << chnl_type;
+    std::string folder_path = oss.str();
+    std::string pre_path = folder_path + "/spectrograms/spectrogram_pre_0.bmp"; // Change "0" to test different spectrograms
+    std::string post_path = folder_path + "/spectrograms/spectrogram_post_0.bmp"; // Change "0" to test different spectrograms
+    
+    cv::Mat pre = cv::imread(pre_path.c_str(), cv::IMREAD_COLOR);
+    cv::Mat post = cv::imread(post_path.c_str(), cv::IMREAD_COLOR);
+    
+    cv::Mat pre_rgba, post_rgba; 
+    cv::cvtColor(pre, pre_rgba, cv::COLOR_BGR2RGBA);
+    cv::cvtColor(post, post_rgba, cv::COLOR_BGR2RGBA);
+    
+    const unsigned char* ptr1 = reinterpret_cast<const unsigned char*>(pre_rgba.data);
+    const unsigned char* ptr2 = reinterpret_cast<const unsigned char*>(post_rgba.data);
+    
+    unsigned char* channel;
+    //Does comparison based on type specifed by user
+    if (cmp_type == 0) {
+        channel = serialCompare(ptr1, ptr2, numPixels);
+    }else if (cmp_type == 1){
+        channel = parallelCompare(ptr1, ptr2, numPixels);
+    }else if (cmp_type  == 1){
+        channel = vectorizedCompare(ptr1, ptr2, numPixels);
+    }else {
+        std::cerr << "Invalid input for compare type \nValid compare types: \nserial=0 \nparallel=1 \nvectorized=2\n"; 
+    }    
+    std::string channel_name = folder_path + "-channel.bmp";
+    createChannelImage(channel, im_rows, im_cols, channel_name); 
+    
+    return 0;
+}
